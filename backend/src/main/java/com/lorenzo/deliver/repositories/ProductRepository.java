eb9237c724b533b55874ae2fa6535af86255d18d
LorenzoBondan <lbbondan@gmail.com>
2023-03-06 17:21:42 -0300

    get products

--- backend/src/main/java/com/lorenzo/deliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/lorenzo/deliver/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.lorenzo.deliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.lorenzo.deliver.entities.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>{
 
+	List<Product> findAllByOrderByNameAsc();
 }
