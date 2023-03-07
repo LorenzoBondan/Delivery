3ed2624914575984fd958a94be7535dfd5b67179
LorenzoBondan <lbbondan@gmail.com>
2023-03-07 13:56:26 -0300

    post orders

--- backend/src/main/java/com/lorenzo/deliver/resources/OrderResource.java
+++ backend/src/main/java/com/lorenzo/deliver/resources/OrderResource.java
@@ -1,13 +1,18 @@
 package com.lorenzo.deliver.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.lorenzo.deliver.dto.OrderDTO;
 import com.lorenzo.deliver.services.OrderService;
 
@@ -22,6 +27,14 @@ public class OrderResource {
 	public ResponseEntity<List<OrderDTO>> findPendingOrders() {
 		List<OrderDTO> list = service.findPendingOrders();	
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert (@Valid @RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);	
 	}
 	
 
--- backend/src/main/java/com/lorenzo/deliver/services/OrderService.java
+++ backend/src/main/java/com/lorenzo/deliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.lorenzo.deliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lorenzo.deliver.dto.OrderDTO;
+import com.lorenzo.deliver.dto.ProductDTO;
 import com.lorenzo.deliver.entities.Order;
+import com.lorenzo.deliver.entities.Product;
+import com.lorenzo.deliver.entities.enums.OrderStatus;
 import com.lorenzo.deliver.repositories.OrderRepository;
+import com.lorenzo.deliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,35 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findPendingOrders(){
 		List<Order> list = repository.findPendingOrders();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order entity = new Order();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new OrderDTO(entity);
+	}
+	
+	
+	private void copyDtoToEntity(OrderDTO dto, Order entity) {
+		entity.setAddress(dto.getAddress());
+		entity.setLatitude(dto.getLatitude());
+		entity.setLongitude(dto.getLongitude());
+		entity.setMoment(Instant.now());
+		entity.setStatus(OrderStatus.PENDING);
+		entity.setTotal(dto.getTotal());
+		
+		for(ProductDTO prodDTO : dto.getProducts() ) {
+			Product product = productRepository.getOne(prodDTO.getId());
+			entity.getProducts().add(product);
+		}
+	}
 }
