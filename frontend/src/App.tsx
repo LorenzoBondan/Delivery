8d76d1b2508791d02d643fb64616d98f4b1804bc
LorenzoBondan <lbbondan@gmail.com>
2023-03-07 15:16:33 -0300

    frontend initial configurations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './assets/styles/custom.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>app</h1>
+    </div>
+  );
+}
+
+export default App;
